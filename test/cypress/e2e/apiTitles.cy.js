8cfed39833e7ed2a8abd5ae701818c5d31fb42d7
Martin Middel <martin@elliat.nl>
2026-07-09 11:04:50 +0200

    fix: remove unused imports from apiTitles to make it work

--- test/cypress/e2e/apiTitles.cy.js
+++ test/cypress/e2e/apiTitles.cy.js
@@ -1,19 +1,51 @@
 // generated from local/apiTitles.xqm
 
-it("GET /api/LIT1367Exodus/title", () => {
-	cy.request({ url: "/api/LIT1367Exodus/title", failOnStatusCode: false }).then((res) => {
-		expect(res.status, `/api/LIT1367Exodus/title responded with ${res.status}`).to.be.lessThan(500);
+describe.only("apiTitles.xqm", () => {
+	it("GET /api/LIT1367Exodus/title", () => {
+		cy.request({
+			url: "/api/LIT1367Exodus/title",
+			failOnStatusCode: false,
+		}).then((res) => {
+			expect(res.status, `/api/LIT1367Exodus/title responded with ${res.status}`).to.be.lessThan(
+				500,
+				"The endpoint should not respond with a server error",
+			);
+			expect(res.status).to.not.equal(
+				405,
+				"The endpoint should not respond with a 405: indicating it is not found in some way",
+			);
+		});
 	});
-});
 
-it("GET /api/LIT1367Exodus/title/json", () => {
-	cy.request({ url: "/api/LIT1367Exodus/title/json", failOnStatusCode: false }).then((res) => {
-		expect(res.status, `/api/LIT1367Exodus/title/json responded with ${res.status}`).to.be.lessThan(500);
+	it("GET /api/LIT1367Exodus/title/json", () => {
+		cy.request({
+			url: "/api/LIT1367Exodus/title/json",
+			failOnStatusCode: false,
+		}).then((res) => {
+			expect(res.status, `/api/LIT1367Exodus/title/json responded with ${res.status}`).to.be.lessThan(
+				500,
+				"The endpoint should not respond with a server error",
+			);
+			expect(res.status).to.not.equal(
+				405,
+				"The endpoint should not respond with a 405: indicating it is not found in some way",
+			);
+		});
 	});
-});
 
-it("GET /api/LIT1367Exodus/a1/title", () => {
-	cy.request({ url: "/api/LIT1367Exodus/a1/title", failOnStatusCode: false }).then((res) => {
-		expect(res.status, `/api/LIT1367Exodus/a1/title responded with ${res.status}`).to.be.lessThan(500);
+	it("GET /api/LIT1367Exodus/a1/title", () => {
+		cy.request({
+			url: "/api/LIT1367Exodus/a1/title",
+			failOnStatusCode: false,
+		}).then((res) => {
+			expect(res.status, `/api/LIT1367Exodus/a1/title responded with ${res.status}`).to.be.lessThan(
+				500,
+				"The endpoint should not respond with a server error",
+			);
+			expect(res.status).to.not.equal(
+				405,
+				"The endpoint should not respond with a 405: indicating it is not found in some way",
+			);
+		});
 	});
 });
